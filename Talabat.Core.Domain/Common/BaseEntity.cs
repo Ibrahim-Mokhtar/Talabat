b27606394468e83f2b6fec36ebcb62e7f4361a97
Ibrahim Mokhtar <ibrahim.mokhtar1611@gmail.com>
2024-10-05 06:15:33 +0300

    Part 02 BaseEntity

---
+++ Talabat.Core.Domain/Common/BaseEntity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Talabat.Core.Domain.Common
+{
+    public abstract class BaseEntity<TKey> where TKey : IEquatable<TKey>
+    {
+        public required TKey Id { get; set; }
+
+        public required string CreatedBy { get; set; }
+        public TimeOnly CreatedOn { get; set; }  /*= DateTime.UtcNow;*/
+        public required string LastModifiedBy { get; set; }
+        public TimeOnly LastModifiedOn { get; set; /*= DateTime.UtcNow;*/
+
+        }
+}
