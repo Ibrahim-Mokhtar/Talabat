3ae57d10ea4b07699d864155d3549fb4af9c654a
Ibrahim Mokhtar <ibrahim.mokhtar1611@gmail.com>
2024-10-05 06:25:44 +0300

    Part 03 Product Module - Entities

---
+++ Talabat.Core.Domain/Entites/Products/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Talabat.Core.Domain.Entites.Products
+{
+    public class Product
+    {
+        public required string Name { get; set; }
+        public required string Description { get; set; }
+        public string? PictureUrl { get; set; }
+        public int Price { get; set; }
+
+        public int? BrandId { get; set; }
+        public virtual ProductBrand? Brand { get; set; } // Foriegn Key --> ProductBrand Entity
+        public int? CategoryId { get; set; }
+        public virtual ProductCategory? Category { get; set; // Foriegn Key --> ProductCategory Entity
+        }
+}
