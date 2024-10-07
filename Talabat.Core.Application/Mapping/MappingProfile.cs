859def9f543411bae4631947c23588951c638cac
Ibrahim Mokhtar <ibrahim.mokhtar1611@gmail.com>
2024-10-08 01:59:05 +0300

    Part 03

--- Talabat.Core.Application/Mapping/MappingProfile.cs
+++ Talabat.Core.Application/Mapping/MappingProfile.cs
@@ -13,6 +13,10 @@ namespace Talabat.Core.Application.Mapping
     {
         public MappingProfile() 
         {
+            CreateMap<Product, ProductToReturnDto>()
+                .ForMember(d => d.Brand, O => O.MapFrom(s => s.Brand.Name))
+                .ForMember(d => d.Category, O => O.MapFrom(s => s.Category.Name));
+
             CreateMap<ProductBrand, BrandDto>();
             CreateMap<ProductCategory, CategoryDto>();
         }
--- Talabat.Infrastructure.Presistence/Repostorie/GenericRepository.cs
+++ Talabat.Infrastructure.Presistence/Repostorie/GenericRepository.cs
@@ -6,6 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Talabat.Core.Domain.Common;
 using Talabat.Core.Domain.Contracts;
+using Talabat.Core.Domain.Entites.Products;
 using Talabat.Infrastructure.Presistence.Data;
 
 namespace Talabat.Infrastructure.Presistence.Repostorie
@@ -15,8 +16,25 @@ namespace Talabat.Infrastructure.Presistence.Repostorie
         where Tkey : IEquatable<Tkey>
     {
         public async Task<IEnumerable<TEntity>> GetAllAsync(bool WithTracking = false)
-       => WithTracking ? await DbContext.Set<TEntity>().ToListAsync() : await DbContext.Set<TEntity>().AsTracking().ToListAsync();
-        public async Task<TEntity?> GetAsync(Tkey id) => await DbContext.Set<TEntity>().FindAsync(id);
+        {
+            //if (typeof(TEntity) == typeof(Product))
+            //{
+            //    if (WithTracking)
+            //       return await DbContext.Set<Product>().Include(P => P.Brand).Include(P => P.Category).ToListAsync();
+            //   return await DbContext.Set<Product>().AsNoTracking().ToListAsync();
+
+            //}
+            if (WithTracking)
+               return await DbContext.Set<TEntity>().ToListAsync();
+           return await DbContext.Set<TEntity>().AsNoTracking().ToListAsync();
+        }
+        public async Task<TEntity?> GetAsync(Tkey id)
+        {
+            //if (typeof(TEntity) == typeof(Product))
+            //    return await DbContext.Set<Product>().Include(P => P.Brand).Include(P => P.Category).FirstOrDefault(P => P.Id == id);
+
+            return await DbContext.Set<TEntity>().FindAsync(id);
+        }
 
         public async Task AddAsync(TEntity entity) => await DbContext.Set<TEntity>().AddAsync(entity);
 
