30633ecbe1f7ee08d0e3fdd4b50805abb52e8d6e
Ibrahim Mokhtar <ibrahim.mokhtar1611@gmail.com>
2024-10-15 13:22:05 +0300

    Part 10 Basket Module Entities

---
+++ Talabat.Core.Domain/Entites/Basket/CustomerBasket.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Talabat.Core.Domain.Entites.Basket
+{
+    public class CustomerBasket
+    {
+        public required string Id { get; set; }
+        public IEnumerable<BasketItem> Items { get; set; }= new List<BasketItem>();
+    }
+}
