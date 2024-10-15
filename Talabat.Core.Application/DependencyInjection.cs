ee3ff7f09e6016fa9ee97a2300ae9b32e2072d7c
Ibrahim Mokhtar <ibrahim.mokhtar1611@gmail.com>
2024-10-15 15:17:21 +0300

    Part 12 Basket Service

--- Talabat.Core.Application/DependencyInjection.cs
+++ Talabat.Core.Application/DependencyInjection.cs
@@ -1,14 +1,19 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using AutoMapper;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Talabat.Core.Application.Abstraction.Services;
+using Talabat.Core.Application.Abstraction.Services.Basket;
 using Talabat.Core.Application.Abstraction.Services.Products;
 using Talabat.Core.Application.Mapping;
 using Talabat.Core.Application.Services;
+using Talabat.Core.Application.Services.Basket;
 using Talabat.Core.Application.Services.Products;
+using Talabat.Core.Domain.Contracts.Infrastructure;
 
 namespace Talabat.Core.Application
 {
@@ -20,7 +25,18 @@ namespace Talabat.Core.Application
             services.AddAutoMapper(typeof(MappingProfile));
             services.AddScoped(typeof(IProductService), typeof(ProductService));
             services.AddScoped(typeof(IServiceManager), typeof(ServiceManager));
-           
+
+            //services.AddScoped(typeof(IBasketServices), typeof(BasketServices));
+            //services.AddScoped(typeof(Func<IBasketServices>), typeof(Func<BasketServices>));
+
+            services.AddScoped(typeof(Func<IBasketServices>), serviceProvider =>
+            {
+                var mapper = serviceProvider.GetRequiredService<IMapper>();
+                var configuration = serviceProvider.GetRequiredService<IConfiguration>();
+                var basketRepository = serviceProvider.GetRequiredService<IBasketRepository>();
+
+                return new BasketServices(basketRepository, mapper, configuration);
+            });
             return services;
         }
     }
--- Talabat.Core.Application/Services/ServiceManager.cs
+++ Talabat.Core.Application/Services/ServiceManager.cs
@@ -1,14 +1,18 @@
 ﻿using AutoMapper;
+using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Talabat.Core.Application.Abstraction.Services;
+using Talabat.Core.Application.Abstraction.Services.Basket;
 using Talabat.Core.Application.Abstraction.Services.Employees;
 using Talabat.Core.Application.Abstraction.Services.Products;
+using Talabat.Core.Application.Services.Basket;
 using Talabat.Core.Application.Services.Employees;
 using Talabat.Core.Application.Services.Products;
+using Talabat.Core.Domain.Contracts.Infrastructure;
 using Talabat.Core.Domain.Contracts.Persistence;
 
 namespace Talabat.Core.Application.Services
@@ -17,17 +21,23 @@ namespace Talabat.Core.Application.Services
     {
         private readonly IUnitOfWork _unitOfWork;
         private readonly IMapper _mapper;
+        private readonly IConfiguration _configuration;
         private readonly Lazy<IProductService> _productService;
         private readonly Lazy<IEmployeeService> _employeeService;
+        private readonly Lazy<IBasketServices> _basketServices;
 
-        public ServiceManager(IUnitOfWork unitOfWork, IMapper mapper)
+        public ServiceManager(IUnitOfWork unitOfWork, IMapper mapper,IConfiguration configuration,Func<IBasketServices> basketServiceFactory)
         {
             _unitOfWork = unitOfWork;
             _mapper = mapper;
+            _configuration = configuration;
             _productService = new Lazy<IProductService>(() => new ProductService(_unitOfWork, _mapper));
             _employeeService=new Lazy<IEmployeeService>(()=>new EmployeeService(_unitOfWork, _mapper));
+            _basketServices = new Lazy<IBasketServices>(basketServiceFactory);
         }
         public IProductService ProductService => _productService.Value;
         public IEmployeeService EmployeeService => _employeeService.Value;
+
+        public IBasketServices BasketServices => _basketServices.Value;
     }
 }
