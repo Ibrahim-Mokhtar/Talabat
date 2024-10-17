2c9cce374c6fd871d0c6d616387df4cbb874dcbe
Ibrahim Mokhtar <ibrahim.mokhtar1611@gmail.com>
2024-10-17 21:30:41 +0300

    Part 01 Identity Module - Entities

---
+++ Talabat.Core.Domain/Entites/Identity/Address.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Talabat.Core.Domain.Entites.Identity
+{
+    public class Address
+    {
+        public int Id { get; set; }
+        public required string FristName { get; set; }
+        public required string LastName { get; set; }
+        public required string Street { get; set; }
+        public required string City { get; set; }
+        public required string Country { get; set; }
+
+        public int UserId { get; set; }
+        public required ApplicationUser AppUser { get; set; }
+    }
+}
