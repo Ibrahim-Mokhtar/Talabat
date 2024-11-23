90756950d832aed359070d7690faf4a6992a6a6b
Ibrahim Mokhtar <ibrahim.mokhtar1611@gmail.com>
2024-11-24 00:58:22 +0200

    Part 05 Order Service - CreateOrder

--- Talabat.Core.Domain/Entites/Orders/OrderItem.cs
+++ Talabat.Core.Domain/Entites/Orders/OrderItem.cs
@@ -8,6 +8,7 @@ namespace Talabat.Core.Domain.Entites.Orders
 {
     public class OrderItem:BaseAuditableEntity<int>
     {
+
         public  required ProductItemOrdered Product { get; set; }
         public decimal Price { get; set; }
         public int Quantity { get; set; }
